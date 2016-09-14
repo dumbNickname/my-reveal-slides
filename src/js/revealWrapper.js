693b8b6d5391e47238e28875be2b12c3b37a16d1
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2016-09-14 18:13:33 +0200

    Added first part of Chrome Dev Tools slides

--- src/js/revealWrapper.js
+++ src/js/revealWrapper.js
@@ -4,9 +4,11 @@ Reveal.initialize({
     controls: true,
     progress: true,
     history: true,
-    center: false,
+    center: true,
+    overview: true,
+    touch: true,
     theme: window.Reveal.getQueryHash().theme, // available themes are in /css/theme
-    transition: 'cube' // default/cube/page/concave/zoom/linear/fade/none
+    transition: 'linear' // default/cube/page/concave/zoom/linear/fade/none
 });
 
 export default Reveal;
